e31d652ed6bf2f58cb1f82aa67ed4723d7873150
Agência Alldigital <rodrigo.costa@agenciaalldigital.com>
2024-01-17 11:54:38 -0300

    .

---
+++ blank.php
@@ -0,0 +1,9 @@
+"require": {
+"ext-pdo": "^7.1",
+"phpmailer/phpmailer": "~6.0",
+"league/plates": "v4.0.0-alpha",
+"coffeecode/uploader": "^1.0",
+"coffeecode/cropper": "^1.0",
+"coffeecode/optimizer": "^1.0",
+"coffeecode/paginator": "^1.0"
+}
